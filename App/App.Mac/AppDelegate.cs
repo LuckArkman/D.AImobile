de9acb3de8443362cdf8f06f3c7a5afa1456a996
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-12-14 09:39:31 -0300

    remove

--- App/App.Mac/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-using Foundation;
-
-namespace App.Mac;
-
-[Register(nameof(AppDelegate))]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
